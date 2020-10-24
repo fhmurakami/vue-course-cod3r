8c1e4fc3e8cb02e937711fb0604a29190be51c87
fhmurakami <fhmurakami@gmail.com>
2020-10-24 20:27:37 -0300

    Add the buttons to control the game

--- project-01/app.js
+++ project-01/app.js
@@ -2,13 +2,18 @@ new Vue({
   el: "#app",
   data: {
     playerLife: 100,
-    monsterLife: 100
+    monsterLife: 100,
+    gameStarted: false
   },
   computed: {
     hasResult() {
       return this.monsterLife <= 0 || this.playerLife <= 0
     }
   },
-  methods: {},
+  methods: {
+    startGame() {
+      this.gameStarted = true
+    }
+  },
   watch: {}
 });
