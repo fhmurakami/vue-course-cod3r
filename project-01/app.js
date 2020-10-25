78e1342d0f6c91da0d69ba9d53171b09679392d2
fhmurakami <fhmurakami@gmail.com>
2020-10-25 15:55:24 -0300

    Implementing attack function

--- project-01/app.js
+++ project-01/app.js
@@ -3,17 +3,55 @@ new Vue({
   data: {
     playerLife: 100,
     monsterLife: 100,
-    gameStarted: false
+    gameStarted: false,
   },
   computed: {
     hasResult() {
-      return this.monsterLife <= 0 || this.playerLife <= 0
-    }
+      return this.monsterLife <= 0 || this.playerLife <= 0;
+    },
   },
   methods: {
     startGame() {
-      this.gameStarted = true
-    }
+      this.gameStarted = true;
+      this.playerLife = 100;
+      this.monsterLife = 100;
+    },
+    endGame() {
+      this.gameStarted = false;
+    },
+    attack(type = "normal") {
+      let playerMin;
+      let playerMax;
+      if (type == "special") {
+        playerMin = 10;
+        playerMax = 20;
+      } else {
+        playerMin = 5;
+        playerMax = 15;
+      }
+
+      let monsterMin = 5;
+      let monsterMax = 20;
+
+      let playerAttack =
+        Math.floor(Math.random() * (playerMax - playerMin + 1)) + playerMin;
+      let monsterAttack =
+        Math.floor(Math.random() * (monsterMax - monsterMin + 1)) + monsterMin;
+
+      this.monsterLife -= playerAttack;
+
+      if (this.monsterLife <= 0) {
+        this.monsterLife = 0;
+        this.gameStarted = false;
+      }
+
+      this.playerLife -= monsterAttack;
+
+      if (this.playerLife <= 0) {
+        this.playerLife = 0;
+        this.gameStarted = false;
+      }
+    },
   },
-  watch: {}
+  watch: {},
 });
