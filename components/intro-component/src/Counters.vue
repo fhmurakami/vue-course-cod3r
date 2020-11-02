94c6ab735c1f6aa40516908f993311c945434f0f
fhmurakami <fhmurakami@gmail.com>
2020-11-02 14:39:49 -0300

    Using component-scoped CSS

---
+++ components/intro-component/src/Counters.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="counters">
+    <h1>Contadores</h1>
+    <app-counter v-for="c in 5" :key="c"/>
+    <button>Test</button>
+  </div>
+</template>
+
+<script>
+import Counter from './Counter.vue'
+export default {
+  components: {
+    'app-counter': Counter
+  }
+}
+</script>
+
+<style>
+
+</style>
