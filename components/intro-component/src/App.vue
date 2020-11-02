482498ad7158e6d1980edfa7b4df8d28493b45af
fhmurakami <fhmurakami@gmail.com>
2020-11-02 11:45:39 -0300

    Create a project using Vue CLI

---
+++ components/intro-component/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    <p>{{ counter }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
