3b621fb5b3969d6216dcd0c0dccfa6314987bda7
fhmurakami <fhmurakami@gmail.com>
2020-11-02 16:33:58 -0300

    Organize the project in components

---
+++ components/componente-desafio/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <app-menu/>
+
+    <app-toolbar/>
+
+    <app-carousel/>
+
+    <app-footer/>
+  </v-app>
+</template>
+
+<script>
+import Menu from './Menu.vue'
+import Toolbar from './Toolbar.vue'
+import Carousel from './Carousel.vue'
+import Footer from './Footer.vue'
+export default {
+  components: {
+    'app-menu': Menu,
+    'app-toolbar': Toolbar,
+    'app-carousel': Carousel,
+    'app-footer': Footer
+  }
+}
+</script>
